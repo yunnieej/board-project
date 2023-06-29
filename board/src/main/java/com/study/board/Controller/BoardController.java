8ea2e50ada347ae448667756f0be2e3221a7f2b4
yunnieej <yunnieej@gamil.com>
2023-06-29 18:00:24 +0900

    pagination

--- board/src/main/java/com/study/board/Controller/BoardController.java
+++ board/src/main/java/com/study/board/Controller/BoardController.java
@@ -3,72 +3,130 @@ package com.study.board.Controller;
 import com.study.board.dto.BoardDTO;
 import com.study.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+// rest api -> 주소상으로 자원을 표현, 식별하는 의미
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board") // board로 시작하는 주소는 이 Controller가 받음
 public class BoardController {
-    private final BoardService boardService;;
+    private final BoardService boardService;
+
     @GetMapping("/save")
-    public String saveForm(){
+    public String saveForm() {
         return "save";
     }
+
+    // model은 controller에서 생성된 데이터를 view로 전달
     // ModelAttribute로 boardDTO클래스 객체를 찾아서
     @PostMapping("/save")
-    public String save(@ModelAttribute BoardDTO boardDTO){
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        System.out.println("객체 받아옴");
         System.out.println("boardDTO = " + boardDTO);
         boardService.save(boardDTO);
         return "index";
     }
 
-    //데이터 가져오면 model객체
-    @GetMapping("/")
-    public String findAll(Model model){
+    //전체 목록을 database 로부터 가져와야하기 때문에 model 객체 사용
+    @GetMapping("/lists")
+    public String findAll(Model model) {
         // DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다.
+        System.out.println("객체 리스트 받아옴");
         List<BoardDTO> boardDTOList = boardService.findAll();
+        // 가져온 데이터 model 객체에 담아야 함.
         model.addAttribute("boardList", boardDTOList); //boardDTOList 데이터를 담은 boardList
         return "list";
     }
 
     @GetMapping("/{id}")
-    public String findById(@PathVariable Long id, Model model){
+    public String findById(@PathVariable Long id, Model model) {
+        // @PageableDefault(page = 1) Pageable pageable
         /*
             해당 게시글의 조회수를 하나 올리고
             게시글 데이터를 가져와서 detail.html에 출력
             -> controller에서 2가지 method 호출
+            id에 맞는 데이터를 database에서 가져와야하기 때문에 model 사용
          */
+
+        System.out.println("상세페이지 출력");
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("board", boardDTO);
+//        model.addAttribute("page", pageable.getPageNumber());
         return "detail";
     }
 
+
     @GetMapping("/update/{id}")
-    public String upDateForm(@PathVariable Long id, Model model){
+    public String upDateForm(@PathVariable("id") Long id, Model model) {
+        System.out.println("아이디 " + id + "번 수정하기");
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("boardUpdate", boardDTO); //attributeName이 html에서 받을 name
         return "update";
     }
 
     @PostMapping("/update")
-    public String update(@ModelAttribute BoardDTO boardDTO, Model model){
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        System.out.println(boardDTO + " 수정 완료");
         BoardDTO board = boardService.update(boardDTO); //수정 반영된 board를 가지고 그냥 detail로 가져가는..
         model.addAttribute("board", board);
+        System.out.println("넘어가는 모델 : "+ model);
         return "detail";
 //        return "redirect:/board/" + boardDTO.getId();
     }
 
     @GetMapping("delete/{id}")
-    public String delete(@PathVariable Long id){
+    public String delete(@PathVariable Long id) {
         boardService.delete(id);
         return "redirect:/board/";
     }
+
+    /***
+     * prev : 이전으로 가는 화살표 표시 여부
+     * next : 다음으로 가는 화살표 표시 여부
+     * startPage : 시작 페이지 번호
+     * currentPage : 현재 페이지 번호
+     * endPage : 끝 페이지 번호
+     * displayPageNum : 한번에 표시할 페이지 개수
+     * totalPosts : 모든 글 개수
+     */
+    // /board/paging?page=1
+    @GetMapping("/paging")
+    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model) {
+        System.out.println("페이징 처리한 게시판 목록 보기");
+        // BoardDTO가 담긴 Page 객체
+        System.out.println("==========================");
+        System.out.println("pageable= " + pageable);
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        // page 갯수 20개
+        // 현재 사용자가 3페이지
+        // 1 2 3
+        // 현재 사용자가 7페이지
+        // 7 8 9
+        // 보여지는 페이지 갯수 3개
+        // 총 페이지 갯수 8개
+
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "paging";
+    }
 }
+
+
 
 
 
--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -4,6 +4,10 @@ import com.study.board.dto.BoardDTO;
 import com.study.board.entity.BoardEntity;
 import com.study.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -26,7 +30,8 @@ public class BoardService {
 
     public List<BoardDTO> findAll(){
         List<BoardEntity> boardEntityList = boardRepository.findAll();
-        // entity로 넘어온 객체를 dto객체로 옮겨닮아서 컨트롤러로 return
+        // repositoy -> service로 넘어오면 대부분 entity
+        // entity로 넘어온 객체를 dto 객체로 옮겨담아서 컨트롤러로 return
         List<BoardDTO> boardDTOList = new ArrayList<>();
         for (BoardEntity boardEntity: boardEntityList){
             boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
@@ -61,5 +66,29 @@ public class BoardService {
 
     public void delete(Long id) {
         boardRepository.deleteById(id);
+    }
+
+    // DB로부터 페이징 처리된 데이터를 가져오는 것
+    public Page<BoardDTO> paging(Pageable pageable) {
+        // page 위치에 있는 값은 0부터 시작함 -> 실제 사용자가 요청한 값에서 하나 빠진 값이 요청되야함.
+        int page = pageable.getPageNumber() - 1;
+        int pageLimit = 3; // 한 페이지에 보여질 글 개수
+        // 한 페이지당 3개씩 글을 보여준 후 정렬 기준은 id 기준으로 내림차순 정렬
+        Page<BoardEntity> boardEntities = boardRepository.findAll(PageRequest.of(page, pageLimit, Sort.by(Sort.Direction.DESC, "id")));
+
+        // Page entity가 제공하는 method
+        System.out.println("boardEntities.getContent() = " + boardEntities.getContent()); // 요청 페이지에 해당하는 글
+        System.out.println("boardEntities.getTotalElements() = " + boardEntities.getTotalElements()); // 전체 글갯수
+        System.out.println("boardEntities.getNumber() = " + boardEntities.getNumber()); // DB로 요청한 페이지 번호
+        System.out.println("boardEntities.getTotalPages() = " + boardEntities.getTotalPages()); // 전체 페이지 갯수
+        System.out.println("boardEntities.getSize() = " + boardEntities.getSize()); // 한 페이지에 보여지는 글 갯수
+        System.out.println("boardEntities.hasPrevious() = " + boardEntities.hasPrevious()); // 이전 페이지 존재 여부
+        System.out.println("boardEntities.isFirst() = " + boardEntities.isFirst()); // 첫 페이지 여부
+        System.out.println("boardEntities.isLast() = " + boardEntities.isLast()); // 마지막 페이지 여부
+
+        //paging 화면목록 : id, writer, title, hits, createdTime
+        Page<BoardDTO> boardDTOS = boardEntities.map(board -> new BoardDTO(board.getId(), board.getBoardWriter(), board.getBoardTitle(), board.getBoardHits(), board.getCreatedTime()));
+
+        return boardDTOS;
     }
 }
